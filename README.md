22500e49c7150c4d0b64c593c1601ca333e3b83a
mhcrnl <mhcrnl@gmail.com>
2018-04-26 09:57:23 +0300

    Thu Apr 26 09:57:22 EEST 2018

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 At the top of the file there should be a short introduction and/ or overview that explains **what** the project is. This description should match descriptions added for package managers (Gemspec, package.json, etc.)
 
+This is a Java Text Editor with syntax highlight base on rsyntaxtextarea-2.0.6.jar library, this is in this package.
+
+![img01.png](img/img01.png) 
+
 ## Code Example
 
 Show what the library does as concisely as possible, developers should be able to figure out **how** your project solves their problem by looking at the code example. Make sure the API you are showing off is obvious, and that your code is short and concise.
--- src/javamhcrnltexteditor2018/TextEditor.java
+++ src/javamhcrnltexteditor2018/TextEditor.java
@@ -5,21 +5,65 @@
  */
 package javamhcrnltexteditor2018;
 
+import com.sun.glass.events.KeyEvent;
+import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import org.fife.ui.rtextarea.*;
+import org.fife.ui.rsyntaxtextarea.*;
 /**
  *
  * @author mhcrnl
  */
 public class TextEditor {
+    
+    //JFrame frame;
+    
+    public JMenuBar createMenuBar() {
+        
+        JMenuBar menuBar = new JMenuBar();
+        JMenu menu, submenu;
+        JMenuItem menuClose,menuItem;
+        
+        menu = new JMenu("File");
+        menu.setMnemonic(KeyEvent.VK_F);
+        menu.getAccessibleContext().setAccessibleDescription("Meniul File");
+        menuBar.add(menu);
+        
+        menuClose = new JMenuItem("Close", KeyEvent.VK_C);
+        menuClose.addActionListener(new ActionListener(){ 
+
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                 System.exit(0);
+            }
+            
+        });
+        menu.add(menuClose);
+        
+        return menuBar;
+    }
     /**
      * Create the GUI and show it.  For thread safety,
      * this method should be invoked from the
      * event-dispatching thread.
      */
     public static void createAndShowGUI(){
+        
+        JPanel cp = new JPanel(new BorderLayout());
+        
+        RSyntaxTextArea textArea = new RSyntaxTextArea(20,60);
+        textArea.setSyntaxEditingStyle(SyntaxConstants.SYNTAX_STYLE_JAVA);
+        textArea.setCodeFoldingEnabled(true);
+        RTextScrollPane sp = new RTextScrollPane(textArea);
+        cp.add(sp);
         //Create and setup the frame
         JFrame frame = new JFrame(JavaMhcrnlTextEditor2018.NAME);
         // set this so your application can easily be stopped/killed
@@ -27,10 +71,11 @@ public class TextEditor {
         // set the jframe size. in a more complicated application you will
         // probably call frame.pack() before displaying it.
         frame.setPreferredSize(new Dimension(800, 600));
+        TextEditor teMenu = new TextEditor();
+        frame.setJMenuBar(teMenu.createMenuBar());
         
-        
-        JLabel label = new JLabel("Salut editor!");
-        frame.getContentPane().add(label);
+        //JLabel label = new JLabel("Salut editor!");
+        frame.setContentPane(cp);//.add(label);
         
         frame.pack();
         // center the frame
